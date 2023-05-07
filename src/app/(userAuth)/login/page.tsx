b5655ec385d8115e696e1ac8b4ddd454f08f0097
Emanuel Pecson <emanpecson@gmail.com>
2023-05-06 22:56:30 -0700

    login page can confirm user exists in db

--- src/app/(userAuth)/login/page.tsx
+++ src/app/(userAuth)/login/page.tsx
@@ -1,74 +1,63 @@
+'use client';
+
 import FormData from 'form-data';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
+
+export async function handleSubmit(event) {
+	event.preventDefault();
+	const formData = new FormData(event.target);
+
+	try {
+		const response = await fetch('../../api/user', {
+			method: 'GET'
+		});
+
+		const { key, users } = await response.json();
+
+		console.log('Success:', users);
+
+		for(let user of users) {
+			if(user.email == formData.get('email') && user.password == formData.get('password')) {
+				console.log('user found');
+
+				return;
+			}
+		}
+		console.log('user not found');
+	}
+	catch(error) {
+		console.error('Error retrieving data', error);
+	}
+}
 
 // then when doing the id, it'll be an arg in params: export default function test({ params })
 export default function Login() {
-	// const [email, setEmail] = useState('');
-	// const [password, setPassword] = useState('');
-	// const [isValid, setIsValid] = useState(false);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [isValid, setIsValid] = useState(false);
 
-	// const handleEmailChange = (event) => {
-	// 	setEmail(event.target.value);
-	// 	setIsValid(event.target.value && password)
-	// }
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+		setIsValid(event.target.value && password)
+	}
 
-	// const handlePasswordChange = (event) => {
-	// 	setPassword(event.target.value);
-	// 	setIsValid(email && event.target.value);
-	// }
-
-	// const handleSubmit = (event) => {
-	// 	event.preventDefault();
-	// 	const formData = new FormData(event.target);
-	// 	// const router = useRouter();
-
-	// 	fetch('/api/users', {
-	// 		method: 'GET'
-	// 	})
-	// 	.then(response => response.json())
-	// 	.then(data => { 
-	// 		console.log('Success:', data);
-	// 		const allUsers = Object.values(data.data);
-	
-	// 		for(let user of allUsers) {
-	// 			if(user.email === formData.get('email') && user.password === formData.get('password')) {
-	// 				console.log('USER FOUND');
-	
-	// 				// router.push('/LeagueMenu/[id]', '/LeagueMenu/${userId}');
-	
-	// 				// router.push({
-	// 				// 	pathname: './LeagueMenu/',
-	// 				// 	query: { id: user.id }
-	// 				// });
-
-	// 				return; // temp
-	// 			}
-	// 		}
-	// 		console.log('INVALID USER CREDENTIALS');
-	
-	// 	})
-	// 	.catch(error => {
-	// 		console.log('Error:', error); 
-	// 		console.log('check')
-	// 	});
-	// }
-
-	const handleSubmit = () => {
-		return;
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+		setIsValid(email && event.target.value);
 	}
 
 	return(
 		<div>
 			<h1>Login to your account!</h1>
 
-			{/* <form onSubmit={handleSubmit}> */}
-			<form>
+			<form onSubmit={handleSubmit}>
+			{/* <form> */}
 				<label>Email:</label>
 				<input 
 					type="email"
 					name="email"
-					// value={email}
-					// onChange={handleEmailChange}
+					value={email}
+					onChange={handleEmailChange}
 					className="appearance-none border rounded w-full py-2 px-4 text-gray-700 bg-gray-200 focus:outline-none focus:shadow-outline"
 					placeholder="Enter email"/>
 
@@ -76,14 +65,14 @@ export default function Login() {
 				<input
 					type="password"
 					name="password"
-					// value={password}
-					// onChange={handlePasswordChange}
+					value={password}
+					onChange={handlePasswordChange}
 					className="appearance-none border rounded w-full py-2 px-4 text-gray-700 bg-gray-200 focus:outline-none focus:shadow-outline"
 					placeholder="Enter password"/>
 
 				<button
 					type="submit"
-					// disabled={!isValid}
+					disabled={!isValid}
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 					Login
 				</button>
