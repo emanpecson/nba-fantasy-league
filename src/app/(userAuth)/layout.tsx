54916648d8c1bd633dff03b29a12a46c2bfef7c0
Emanuel Pecson <emanpecson@gmail.com>
2023-06-07 19:19:07 -0700

    replace (userAuth) w/ (auth)

--- src/app/(userAuth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-// import '../globals.css' // maybe i don't have to import if already imported from root layout
-
-export default function Layout({ children }: { children: React.ReactNode; }) {
-	return(
-		<div className="box">
-			{ children }
-		</div>
-	)
-}
