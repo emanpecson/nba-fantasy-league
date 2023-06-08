143886ae92ed228bb199bebe62c11341d62fe4bf
Emanuel Pecson <emanpecson@gmail.com>
2023-06-07 20:58:17 -0700

    add Player model

---
+++ src/models/Player.ts
@@ -0,0 +1,22 @@
+interface Player {
+	_id?: string
+    name: string,
+    ftsy_pts: number,
+    pts: number,
+    ast: number,
+    reb: number,
+    fg_pct: number,
+    fg3_pct: number,
+    ft_pct: number,
+    stl: number,
+    blk: number,
+    tov: number,
+    min: number,
+    age: number,
+    team: string
+    pos: string,
+    height: string,
+    weight: string,
+    last_attended: string,
+    country: string
+}
