efabbc22d59874a2f0df90b0281074bff281b7d6
Emanuel Pecson <emanpecson@gmail.com>
2023-06-10 22:19:50 -0700

    fix button content types as React.ReactNode

--- src/components/CommonButton.tsx
+++ src/components/CommonButton.tsx
@@ -1,10 +1,10 @@
-export default function CommonButton({ text, func }: { text: string, func: React.FC }) {
+export default function CommonButton({ children, func }: { children: React.ReactNode, func: React.FC }) {
 	return (
 		<button 
 			className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
 			onClick={func}
 		>
-			{ text }
+			{ children }
 		</button>
 	)
 }
--- src/components/RouteButton.tsx
+++ src/components/RouteButton.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 
-export default function RouteButton({ text, route }: { text: string, route: string }) {
+export default function RouteButton({ children, route }: { children: React.ReactNode, route: string }) {
 	const router = useRouter();
 
 	return (
@@ -10,7 +10,7 @@ export default function RouteButton({ text, route }: { text: string, route: stri
 			className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
 			onClick={() => { router.push(route) }}
 		>
-			{ text }
+			{ children }
 		</button>
 	)
 }
