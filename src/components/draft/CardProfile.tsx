4d21d9bb471c143c76292aaebcb8f5c8fd8f447b
Emanuel Pecson <emanpecson@gmail.com>
2023-07-10 21:54:16 -0700

    change code spacing

--- src/components/draft/CardProfile.tsx
+++ src/components/draft/CardProfile.tsx
@@ -16,21 +16,21 @@ export default function CardProfile({
 				<p>Name: { card?.name }</p>
 				<p>Fantasy PPG: { card?.fantasyPpg }</p>
 				<p>PPG: { card?.ppg }</p>
-				<p>APG: {card?.apg }</p>
-				<p>RPG: {card?.rpg }</p>
-				<p>FG%: {card?.fgPct }</p>
-				<p>3P%: {card?.fg3Pct }</p>
-				<p>SPG: {card?.spg }</p>
-				<p>BPG: {card?.bpg }</p>
-				<p>TPG: {card?.tpg }</p>
-				<p>MPG: {card?.mpg }</p>
-				<p>Age: {card?.age }</p>
-				<p>Position: {card?.position }</p>
-				<p>Team: { card?.team }</p>
-				<p>Height: {card?.height }</p>
-				<p>Weight: {card?.weight }</p>
-				<p>Last attended: {card?.lastAttended }</p>
-				<p>Country: {card?.country }</p>
+				<p>APG: { card?.apg }</p>
+				<p>RPG: { card?.rpg }</p>
+				<p>FG%: { card?.fgPct }</p>
+				<p>3P%: { card?.fg3Pct }</p>
+				<p>SPG: { card?.spg }</p>
+				<p>BPG: { card?.bpg }</p>
+				<p>TPG: { card?.tpg }</p>
+				<p>MPG: { card?.mpg }</p>
+				<p>Age: { card?.age }</p>
+				<p>Position: { card?.position }</p>
+				<p>Team: { card?.team ? card.team : 'N/A' }</p>
+				<p>Height: { card?.height }</p>
+				<p>Weight: { card?.weight }</p>
+				<p>Last attended: { card?.lastAttended ? card.lastAttended : 'N/A' }</p>
+				<p>Country: { card?.country ? card.country : 'N/A' }</p>
 			</div>
 		</Modal>
 	);
