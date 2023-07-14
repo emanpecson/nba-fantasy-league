8cf214967db4f2049c116fa97e85ed8eb97da8aa
Emanuel Pecson <emanpecson@gmail.com>
2023-07-14 02:29:16 -0700

    add prompt for no data

--- src/components/draft/DraftTable.tsx
+++ src/components/draft/DraftTable.tsx
@@ -1,87 +1,111 @@
-import { Card } from "@prisma/client"
-import { useState } from "react";
-import CardModal from "./CardModal";
-import Roster from "@/models/Roster";
+import { Card } from '@prisma/client';
+import { useState } from 'react';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import CardModal from './CardModal';
+import Roster from '@/models/Roster';
 
 export default function DraftTable({
-	playerCards,
-	isLoading,
-	roster,
-	setRoster,
+  playerCards,
+  isLoading,
+  roster,
+  setRoster,
 }: {
-	playerCards: Card[];
-	isLoading: boolean;
-	roster: Roster;
-	setRoster: (roster: Roster) => void;
+  playerCards: Card[];
+  isLoading: boolean;
+  roster: Roster;
+  setRoster: (roster: Roster) => void;
 }) {
-	const [cardModalIsOpen, setCardModalIsOpen] = useState(false);
-	const [cardProfile, setCardProfile] = useState<Card | null>(null);
+  const [cardModalIsOpen, setCardModalIsOpen] = useState(false);
+  const [cardProfile, setCardProfile] = useState<Card | null>(null);
 
-	const loadingRows: JSX.Element[] = [];
-	for(let i = 0; i < 6; i++) {
-		loadingRows.push(
-			<tr key={i} className="animate-pulse bg-slate-100 sm:rounded-lg">
-				<td className="py-7 pl-4 pr-3 sm:pl-6 lg:pl-8"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-				<td className="px-3 py-7"></td>
-			</tr>
-		)
-	}
+	// or optionally replace the entire table and make up some shape
+  const loadingRows: JSX.Element[] = [];
+  for (let i = 0; i < 6; i++) {
+    loadingRows.push(
+      <tr key={i} className="justify-items-center">
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+        <td className="h-14 w-6"><div className="container mx-auto animate-pulse rounded-full bg-[#f5f5f5] h-5 w-5/6"></div></td>
+      </tr>
+    );
+  }
 
-	function handleRowClick(card: Card) {
-		setCardProfile(card);
-		setCardModalIsOpen(true);
-	}
+  function handleRowClick(card: Card) {
+    setCardProfile(card);
+    setCardModalIsOpen(true);
+  }
 
   return (
-		<div className="pt-4">
-			<div className="px-4 sm:px-6 lg:px-8 h-96 overflow-auto shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg bg-white">
-				<div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
-					<div className="inline-block min-w-full py-2 align-middle">
-						<table className="min-w-full divide-y divide-gray-300 relative">
-							<thead>
-								<tr>
-									<th className="sticky top-0 bg-white opacity-90 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Name</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Fantasy</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Points</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Assists</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Rebounds</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Steals</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Blocks</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Turnovers</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Team</th>
-									<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Position</th>
-								</tr>
-							</thead>
-							<tbody className="divide-y divide-gray-200 bg-white">
-								{ !isLoading && playerCards.map((card: Card) => (
-									<tr key={card.id} onClick={() => handleRowClick(card)} className="cursor-pointer hover:bg-gray-50">
-										<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{ card.name }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.fantasyPpg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.ppg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.apg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.rpg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.spg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.bpg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.tpg }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.team ? card.team : 'N/A' }</td>
-										<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{ card.position }</td>
+    <div className="pt-4">
+      <div className="px-4 sm:px-6 lg:px-8 h-96 overflow-auto shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg bg-white">
+        <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8 h-full">
+          <div className="inline-block min-w-full py-2 align-middle h-full">
+						{ playerCards.length > 0 && 
+							<table className="min-w-full divide-y divide-gray-300 relative">
+								<thead>
+									<tr>
+										<th className="sticky top-0 bg-white opacity-90 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Name</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Fantasy</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Points</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Assists</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Rebounds</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Steals</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Blocks</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Turnovers</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Team</th>
+										<th className="sticky top-0 bg-white opacity-90 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Position</th>
 									</tr>
-								)) }
-								{ isLoading && loadingRows }
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-			<CardModal card={cardProfile} cardModalIsOpen={cardModalIsOpen} setCardModalIsOpen={setCardModalIsOpen} roster={roster} setRoster={setRoster} />
+								</thead>
+								<tbody className="divide-y divide-gray-200 bg-white">
+									{!isLoading &&
+										playerCards.map((card: Card) => (
+											<tr
+												key={card.id}
+												onClick={() => handleRowClick(card)}
+												className="cursor-pointer hover:bg-gray-50">
+												<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{card.name}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.fantasyPpg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.ppg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.apg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.rpg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.spg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.bpg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.tpg}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.team ? card.team : 'N/A'}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.position}</td>
+											</tr>
+										))}
+									{isLoading && loadingRows}
+								</tbody>
+							</table>
+						}
+						{ playerCards.length == 0 &&
+							<div className="h-full">
+								<div className="h-full flex justify-center items-center">
+									<div className="flex bg-yellow-100 space-x-2 items-center justify-center rounded-full w-32 h-10 font-semibold text-yellow-700">
+										<ExclamationTriangleIcon className="h-6 w-6" />
+										<p>No data</p>
+									</div>
+								</div>
+							</div>
+						}
+          </div>
+        </div>
+      </div>
+      <CardModal
+        card={cardProfile}
+        cardModalIsOpen={cardModalIsOpen}
+        setCardModalIsOpen={setCardModalIsOpen}
+        roster={roster}
+        setRoster={setRoster}
+      />
     </div>
-  )
+  );
 }
