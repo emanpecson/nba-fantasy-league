4ae14160502003d99ab88d6880feda30f5872ebf
Emanuel Pecson <emanpecson@gmail.com>
2023-07-23 17:36:53 -0700

    add rounds to draft order

--- src/components/draft/PickOrder.tsx
+++ src/components/draft/PickOrder.tsx
@@ -1,13 +1,14 @@
 export default function PickOrder() {
-	const teamPlaceholder = ['Lakers', 'Warriors', 'Thunder'];
+	const teamStack = ['Lakers', 'Warriors', 'Thunder', 'Heat', 'Nuggets', 'My Team'];
 
 	const rounds: JSX.Element[] = [];
-  for (let i = 0; i < 3; i++) {
+  for (let i = 1; i <= 10; i++) {
     rounds.push(
 			<ul className="divide-y">
+				<p className="py-3 font-semibold flex justify-center">Round { i }</p>
 				{
-					teamPlaceholder.map((team) => (
-						<li className="p-4">{team}</li>
+					teamStack.map((team) => (
+						<li className="p-4 flex">{ team }</li>
 					))
 				}
 			</ul>
@@ -15,8 +16,8 @@ export default function PickOrder() {
   }
 
 	return (
-		<div className="bg-white h-full rounded-lg drop-shadow-lg">
-			<div className="divide-y divide-gray-400">
+		<div className="bg-white rounded-lg drop-shadow-lg">
+			<div className="divide-y divide-gray-400 h-[48rem] overflow-auto">
 				{ rounds }
 			</div>
 		</div>
@@ -27,4 +28,8 @@ export default function PickOrder() {
 	algorithm:
 		ima have my own team
 
+		for whatever the bot that is choosing, make it choose:
+			a. top player option
+			b. if can't place top player option for any roster spots
+				then choose by next best for available position
 */
