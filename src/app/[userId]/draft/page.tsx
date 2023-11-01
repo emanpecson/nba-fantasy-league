3078e24e01a17668aa97ccb6957d22afc24790ce
Emanuel Pecson <emanpecson@gmail.com>
2023-10-31 18:41:11 -0700

    style: :lipstick: update formatting

--- src/app/[userId]/draft/page.tsx
+++ src/app/[userId]/draft/page.tsx
@@ -9,268 +9,274 @@ import RosterView from '@/components/draft/roster/RosterView';
 import DraftSearch from '@/components/draft/DraftSearch';
 import TeamCombobox from '@/components/draft/TeamCombobox';
 import PositionSelect from '@/components/draft/PositionSelect';
-import isCurrentTeamAndRound from '@/lib/draft/isCurrentTeamAndRound';
+import isCurrentTeamAndRound from '@/util/draft/isCurrentTeamAndRound';
 import RouteButton from '@/components/RouteButton';
 import Modal from '@/components/Modal';
 import seedrandom from 'seedrandom';
 import Roster from '@/models/Roster';
 
 export default function Draft({ params }: { params: { userId: string } }) {
-	// displayed data
+  // displayed data
   const [playerCards, setPlayerCards] = useState<Card[]>([]);
-	const [cpuPlayerCards, setCpuPlayerCards] = useState<Card[]>([]);
-	const [myTeam, setMyTeam] = useState<DraftTeam>(new DraftTeam('MyTeam', true));
-	const [teams, setTeams] = useState<DraftTeam[]>([
-		myTeam,
-		new DraftTeam('Warriors', false),
-		new DraftTeam('Wolves', false),
-		new DraftTeam('Lakers', false),
-		new DraftTeam('Heat', false),
-		new DraftTeam('Nuggets', false),
-		new DraftTeam('Blazers', false),
-		new DraftTeam('Pelicans', false),
-	]);
-	const [cardPicks, setCardPicks] = useState<Card[]>([]);
+  const [cpuPlayerCards, setCpuPlayerCards] = useState<Card[]>([]);
+  const [myTeam, setMyTeam] = useState<DraftTeam>(new DraftTeam('MyTeam', true));
+  const [teams, setTeams] = useState<DraftTeam[]>([
+    myTeam,
+    new DraftTeam('Warriors', false),
+    new DraftTeam('Wolves', false),
+    new DraftTeam('Lakers', false),
+    new DraftTeam('Heat', false),
+    new DraftTeam('Nuggets', false),
+    new DraftTeam('Blazers', false),
+    new DraftTeam('Pelicans', false),
+  ]);
+  const [cardPicks, setCardPicks] = useState<Card[]>([]);
 
-	const teamDraftOrder: DraftTeam[][] = [];	
-	for(let i = 0; i < 10; i++) {
-		const seed = `${i+100}`;
-		const rng = seedrandom(seed);
-		const comparator = () => { return rng() - 0.5 }
+  let count = 0;
+  const [loadedPlayersCount, setLoadedPlayersCount] = useState(count);
+  useEffect(() => {
+    console.log('count change:', loadedPlayersCount);
+    setLoadedPlayersCount(count);
+  }, [count]);
+  const totalPlayersToLoad = teams.length * 10; // each team has 10 players
 
-		const reorderedTeams = [...teams].sort(comparator);
+  const teamDraftOrder: DraftTeam[][] = [];
+  for (let i = 0; i < 10; i++) {
+    const seed = `${i + 100}`;
+    const rng = seedrandom(seed);
+    const comparator = () => {
+      return rng() - 0.5;
+    };
 
-		const round: Array<DraftTeam> = [];
-		for(const team of reorderedTeams) {
-			round.push(team);
-		}
-		teamDraftOrder.push(round);
-	}
+    const reorderedTeams = [...teams].sort(comparator);
 
-	// filters
+    const round: Array<DraftTeam> = [];
+    for (const team of reorderedTeams) {
+      round.push(team);
+    }
+    teamDraftOrder.push(round);
+  }
+
+  // filters
   const [searchInput, setSearchInput] = useState('');
   const [teamFilter, setTeamFilter] = useState('');
   const [positionFilter, setPositionFilter] = useState('');
 
-	// states
+  // states
   const [isLoadingTable, setIsLoadingTable] = useState(false);
-	const [teamPicking, setTeamPicking] = useState(teamDraftOrder[0][0]); // first round, first team
-	const [draftIndex, setDraftIndex] = useState(0);
-	const [leagueHomeRoute, setLeagueHomeRoute] = useState('');
-	const [isOpenWrapupModal, setIsOpenWrapupModal] = useState(false);
-	const [isLoadingWrapup, setIsLoadingWrapup] = useState(false);
-	const [totalPicks, setTotalPicks] = useState(80);
-	const [draftFinished, setDraftFinished] = useState(false);
+  const [teamPicking, setTeamPicking] = useState(teamDraftOrder[0][0]); // first round, first team
+  const [draftIndex, setDraftIndex] = useState(0);
+  const [leagueHomeRoute, setLeagueHomeRoute] = useState('');
+  const [isOpenWrapupModal, setIsOpenWrapupModal] = useState(false);
+  const [isLoadingWrapup, setIsLoadingWrapup] = useState(false);
+  const [totalPicks, setTotalPicks] = useState(80);
+  const [draftFinished, setDraftFinished] = useState(false);
 
-	async function handleFilter() {
-		setIsLoadingTable(true);
+  async function handleFilter() {
+    setIsLoadingTable(true);
 
-		const res = await fetch(
-			`/api/card?searchInput=${searchInput}&team=${teamFilter}&position=${positionFilter}`,
-			{ method: 'GET' }
-		);
-		const { cards } = await res.json();
-		console.log('filtered cards:', cards);
-		setPlayerCards(cards);
+    const res = await fetch(
+      `/api/card?searchInput=${searchInput}&team=${teamFilter}&position=${positionFilter}`,
+      { method: 'GET' }
+    );
+    const { cards } = await res.json();
+    console.log('filtered cards:', cards);
+    setPlayerCards(cards);
 
-		setIsLoadingTable(false);
-	};
+    setIsLoadingTable(false);
+  }
 
-	async function getCpuPlayerCards() {
-		const res = await fetch(
-			'/api/card',
-			{ method: 'GET' }
-		);
-		const { cards } = await res.json();
-		setCpuPlayerCards(cards);
-	}
+  async function getCpuPlayerCards() {
+    const res = await fetch('/api/card', { method: 'GET' });
+    const { cards } = await res.json();
+    setCpuPlayerCards(cards);
+  }
 
   useEffect(() => {
     handleFilter();
-		getCpuPlayerCards();
+    getCpuPlayerCards();
   }, [teamFilter, positionFilter, searchInput]);
 
-	// post in db + route to new page
-	async function finishDraft(teams: DraftTeam[]) {
-		console.log('draft finished');
-		setDraftFinished(true);
-		setIsOpenWrapupModal(true);
+  // post in db + route to new page
+  async function finishDraft(teams: DraftTeam[]) {
+    console.log('draft finished');
+    setDraftFinished(true);
+    setIsOpenWrapupModal(true);
 
-		try {
-			setIsLoadingWrapup(true);
+    try {
+      setIsLoadingWrapup(true);
 
-			// post new league (w/ user id)
-			const res = await fetch('/api/league', {
-				method: 'POST',
-				body: JSON.stringify({
-					name: 'tempName',
-					mode: 'tempMode',
-					userId: params.userId
-				} as League),
-			});
-			const { data } = await res.json();
-			const createdLeague: League = data;
+      // post new league (w/ user id)
+      const res = await fetch('/api/league', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'tempName',
+          mode: 'tempMode',
+          userId: params.userId,
+        } as League),
+      });
+      const { data } = await res.json();
+      const createdLeague: League = data;
 
-			if(!createdLeague)
-				throw Error('Unable to post league');
+      if (!createdLeague) throw Error('Unable to post league');
 
-			// post all teams (w/ league id)
-			for(const team of teams) {
-				console.log('posting team:', team.name);
-				const res = await fetch('/api/team', {
-					method: 'POST',
-					body: JSON.stringify({
-						isUser: team.name === 'MyRoster' ? true : false,
-						name: team.name,
-						leagueId: createdLeague.id
-					} as Team),
-				});
-				const { data } = await res.json();
-				const createdTeam: Team = data;
+      // post all teams (w/ league id)
+      for (const team of teams) {
+        console.log('posting team:', team.name);
+        const res = await fetch('/api/team', {
+          method: 'POST',
+          body: JSON.stringify({
+            isUser: team.name === 'MyRoster' ? true : false,
+            name: team.name,
+            leagueId: createdLeague.id,
+          } as Team),
+        });
+        const { data } = await res.json();
+        const createdTeam: Team = data;
 
-				if(!createdTeam)
-					throw Error(`Unable to post team: ${team.name}`);
+        if (!createdTeam) throw Error(`Unable to post team: ${team.name}`);
 
-				const postPlayer = async (card: Card, isStarter: boolean) => {
-					const res = await fetch('/api/player', {
-						method: 'POST',
-						body: JSON.stringify({
-							isStarter,
-							fantasyTeamId: createdTeam.id,
-							cardId: card?.id,
-						} as Player),
-					});
-					const { data } = await res.json();
-					const createdPlayer: Player = data;
+        const postPlayer = async (card: Card, isStarter: boolean) => {
+          const res = await fetch('/api/player', {
+            method: 'POST',
+            body: JSON.stringify({
+              isStarter,
+              fantasyTeamId: createdTeam.id,
+              cardId: card?.id,
+            } as Player),
+          });
+          const { data } = await res.json();
+          const createdPlayer: Player = data;
 
-					if(!createdPlayer)
-						throw Error(`Unable to post player: ${card?.name}`);
-				}
+          if (!createdPlayer) throw Error(`Unable to post player: ${card?.name}`);
 
-				// post all players (associated w/ team + card)
-				for(const rosterSpot of Object.values(team.roster.starters)) {
-					await postPlayer(rosterSpot as Card, true);
-				}
-				for(const rosterSpot of Object.values(team.roster.bench)) {
-					await postPlayer(rosterSpot as Card, false);
-				}
-			}
+          // const count = loadedPlayersCount;
+          // setLoadedPlayersCount(count+1);
+          count++;
+        };
 
-			// route to league home page
-			setLeagueHomeRoute(`${params.userId}/${createdLeague.id}/home`);
+        // post all players (associated w/ team + card)
+        for (const rosterSpot of Object.values(team.roster.starters)) {
+          await postPlayer(rosterSpot as Card, true);
+        }
+        for (const rosterSpot of Object.values(team.roster.bench)) {
+          await postPlayer(rosterSpot as Card, false);
+        }
+      }
 
-			setIsLoadingWrapup(false);
-		}
-		catch(error) {
-			console.error(error);
-		}
-	}
+      // route to league home page
+      setLeagueHomeRoute(`${params.userId}/${createdLeague.id}/home`);
 
-	// update teams + draft order
-	async function handleSetTeams(teams: DraftTeam[]) {
-		setTeams(teams);
+      setIsLoadingWrapup(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-		// set curr team picking
-		for(let round_i = 0; round_i < teamDraftOrder.length; round_i++) {
-			for(let team_i = 0; team_i < teamDraftOrder[round_i].length; team_i++) {
-				if(isCurrentTeamAndRound(round_i, team_i, draftIndex+1, teams.length)) {
-					setTeamPicking(teamDraftOrder[round_i][team_i]);
-				}
-			}
-		}
+  // update teams + draft order
+  async function handleSetTeams(teams: DraftTeam[]) {
+    setTeams(teams);
 
-		if(!draftFinished && draftIndex+1 === (teamDraftOrder.length * teamDraftOrder[0].length))
-			await finishDraft(teams);
+    // set curr team picking
+    for (let round_i = 0; round_i < teamDraftOrder.length; round_i++) {
+      for (let team_i = 0; team_i < teamDraftOrder[round_i].length; team_i++) {
+        if (isCurrentTeamAndRound(round_i, team_i, draftIndex + 1, teams.length)) {
+          setTeamPicking(teamDraftOrder[round_i][team_i]);
+        }
+      }
+    }
 
-		setDraftIndex(draftIndex+1);
-	}
+    if (!draftFinished && draftIndex + 1 === teamDraftOrder.length * teamDraftOrder[0].length)
+      await finishDraft(teams);
 
-	function isAvailableCard(card: Card) {
-		for(const team of teams) {
-			for(const rosterSpot of Object.values(team.roster.starters)) {
-				if(rosterSpot && rosterSpot.id === card.id)
-					return false;
-			}
-			for(const rosterSpot of Object.values(team.roster.bench)) {
-				if(rosterSpot && rosterSpot.id === card.id)
-					return false;
-			}
-		}
-		return true;
-	}
+    setDraftIndex(draftIndex + 1);
+  }
 
-	function getAvailableRosterSpot(roster: Roster, position: string) {
-		// check starters
-		if(!roster.starters.pg && position.includes('G')) return { isStarting: true, position: 'pg' };
-		if(!roster.starters.sg && position.includes('G')) return { isStarting: true, position: 'sg' };
-		if(!roster.starters.sf && position.includes('F')) return { isStarting: true, position: 'sf' };
-		if(!roster.starters.pf && position.includes('F')) return { isStarting: true, position: 'pf' };
-		if(!roster.starters.c && position.includes('C')) return { isStarting: true, position: 'c' };
+  function isAvailableCard(card: Card) {
+    for (const team of teams) {
+      for (const rosterSpot of Object.values(team.roster.starters)) {
+        if (rosterSpot && rosterSpot.id === card.id) return false;
+      }
+      for (const rosterSpot of Object.values(team.roster.bench)) {
+        if (rosterSpot && rosterSpot.id === card.id) return false;
+      }
+    }
+    return true;
+  }
 
-		// check bench
-		if(!roster.bench.pg && position.includes('G')) return { isStarting: false, position: 'pg' };
-		if(!roster.bench.sg && position.includes('G')) return { isStarting: false, position: 'sg' };
-		if(!roster.bench.sf && position.includes('F')) return { isStarting: false, position: 'sf' };
-		if(!roster.bench.pf && position.includes('F')) return { isStarting: false, position: 'pf' };
-		if(!roster.bench.c && position.includes('C')) return { isStarting: false, position: 'c' };
-		
-		// otherwise, no valid spot for this position
-		return null;
-	}
+  function getAvailableRosterSpot(roster: Roster, position: string) {
+    // check starters
+    if (!roster.starters.pg && position.includes('G')) return { isStarting: true, position: 'pg' };
+    if (!roster.starters.sg && position.includes('G')) return { isStarting: true, position: 'sg' };
+    if (!roster.starters.sf && position.includes('F')) return { isStarting: true, position: 'sf' };
+    if (!roster.starters.pf && position.includes('F')) return { isStarting: true, position: 'pf' };
+    if (!roster.starters.c && position.includes('C')) return { isStarting: true, position: 'c' };
 
-	/*
+    // check bench
+    if (!roster.bench.pg && position.includes('G')) return { isStarting: false, position: 'pg' };
+    if (!roster.bench.sg && position.includes('G')) return { isStarting: false, position: 'sg' };
+    if (!roster.bench.sf && position.includes('F')) return { isStarting: false, position: 'sf' };
+    if (!roster.bench.pf && position.includes('F')) return { isStarting: false, position: 'pf' };
+    if (!roster.bench.c && position.includes('C')) return { isStarting: false, position: 'c' };
+
+    // otherwise, no valid spot for this position
+    return null;
+  }
+
+  /*
 		if curr teamPicking is a cpu, automate process by auto-selecting next
 		best player option
 	*/
-	async function cpuSelect() {
-		const teamsTemp = [...teams];
+  async function cpuSelect() {
+    const teamsTemp = [...teams];
 
-		for(const card of cpuPlayerCards) {
-			const availableRosterSpot = getAvailableRosterSpot(teamPicking.roster, card.position);
-			if(isAvailableCard(card) && availableRosterSpot) {
-				const rosterTemp = {...teamPicking.roster} as Roster;
+    for (const card of cpuPlayerCards) {
+      const availableRosterSpot = getAvailableRosterSpot(teamPicking.roster, card.position);
+      if (isAvailableCard(card) && availableRosterSpot) {
+        const rosterTemp = { ...teamPicking.roster } as Roster;
 
-				if(availableRosterSpot.isStarting) {
-					rosterTemp.starters[availableRosterSpot.position as keyof typeof rosterTemp.starters] = card;
-				} else {
-					rosterTemp.bench[availableRosterSpot.position as keyof typeof rosterTemp.bench] = card;
-				}
+        if (availableRosterSpot.isStarting) {
+          rosterTemp.starters[availableRosterSpot.position as keyof typeof rosterTemp.starters] =
+            card;
+        } else {
+          rosterTemp.bench[availableRosterSpot.position as keyof typeof rosterTemp.bench] = card;
+        }
 
-				console.log(`${teamPicking.name} picked ${card.name}`);
+        console.log(`${teamPicking.name} picked ${card.name}`);
 
-				teamPicking.setRoster(rosterTemp);
+        teamPicking.setRoster(rosterTemp);
 
-				const tempCardPicks = [...cardPicks, card];
-				setCardPicks(tempCardPicks);
+        const tempCardPicks = [...cardPicks, card];
+        setCardPicks(tempCardPicks);
 
-				break;
-			}
-		}
+        break;
+      }
+    }
 
-		// after roster set, set team
-		handleSetTeams(teamsTemp);
-	}
+    // after roster set, set team
+    handleSetTeams(teamsTemp);
+  }
 
-	useEffect(() => {
-		if(!teamPicking.isUser) {
-			setTimeout(() => {
-				cpuSelect();
-			}, 500);
-		}
-	}, [teams])
+  useEffect(() => {
+    if (!teamPicking.isUser) {
+      setTimeout(() => {
+        cpuSelect();
+      }, 500);
+    }
+  }, [teams]);
 
   return (
     <div>
       <div className="flex flex-row">
         <div className="basis-1/5 pt-2 pl-3">
-					<PickOrder 
-						teamDraftOrder={teamDraftOrder}
-						draftIndex={draftIndex}
-						cardPicks={cardPicks}
-						totalPicks={totalPicks}
-						teams={teams}
-					/>
-				</div>
+          <PickOrder
+            teamDraftOrder={teamDraftOrder}
+            draftIndex={draftIndex}
+            cardPicks={cardPicks}
+            totalPicks={totalPicks}
+            teams={teams}
+          />
+        </div>
 
         <div className="basis-4/5 px-3">
           <div className="flex flex-row space-x-1.5">
@@ -285,33 +291,38 @@ export default function Draft({ params }: { params: { userId: string } }) {
             </div>
           </div>
 
-					<div>
-						<DraftTable
-							playerCards={playerCards}
-							isLoading={isLoadingTable}
-							teams={teams}
-							setTeams={handleSetTeams}
-							teamPicking={teamPicking}
-							cardPicks={cardPicks}
-							setCardPicks={setCardPicks}
-						/>
-					</div>
+          <div>
+            <DraftTable
+              playerCards={playerCards}
+              isLoading={isLoadingTable}
+              teams={teams}
+              setTeams={handleSetTeams}
+              teamPicking={teamPicking}
+              cardPicks={cardPicks}
+              setCardPicks={setCardPicks}
+            />
+          </div>
 
-					<div>
-          	<RosterView roster={myTeam.roster} />
-					</div>
+          <div>
+            <RosterView roster={myTeam.roster} />
+          </div>
         </div>
       </div>
-			<Modal isOpen={isOpenWrapupModal} onClose={() => { setIsOpenWrapupModal(false) }}>
-				{ isLoadingWrapup &&
-					<p>Loading...</p>
-				}
-				{ !isLoadingWrapup &&		
-					<RouteButton route={leagueHomeRoute}>
-						league home route
-					</RouteButton>
-				}
-			</Modal>
+      <Modal
+        isOpen={isOpenWrapupModal}
+        onClose={() => {
+          setIsOpenWrapupModal(false);
+        }}>
+        {isLoadingWrapup && (
+          <div>
+            <p>Loading...</p>
+            <p>
+              {loadedPlayersCount}/{totalPlayersToLoad}
+            </p>
+          </div>
+        )}
+        {!isLoadingWrapup && <RouteButton route={leagueHomeRoute}>league home route</RouteButton>}
+      </Modal>
     </div>
   );
 }
