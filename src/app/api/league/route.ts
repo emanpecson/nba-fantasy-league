226ae1b1081c60fbc4cb143454ce5fa32cc27467
Emanuel Pecson <emanpecson@gmail.com>
2023-11-02 15:34:27 -0700

    feat: :sparkles: update league route to get all leagues of a user

--- src/app/api/league/route.ts
+++ src/app/api/league/route.ts
@@ -1,40 +1,47 @@
-import { League, PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import { League, PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-	const query = {
-		id: req.nextUrl.searchParams.get('id')
-	}
+  const query = {
+    leagueId: req.nextUrl.searchParams.get('leagueId'),
+    userId: req.nextUrl.searchParams.get('userId'),
+  };
 
-	try {
-		if(query.id) {
-			const data = await prisma.league.findUnique({
-				where: {
-					id: query.id,
-				}
-			});
+  try {
+    if (query.leagueId) {
+      const data = await prisma.league.findUnique({
+        where: {
+          id: String(query.leagueId),
+        },
+      });
 
-			return NextResponse.json({ data }, { status: 200 });
-		}
-	}
-	catch(error) {
-		console.error('Error:', error);
-		return NextResponse.json(null, { status: 500 });
-	}
+      return NextResponse.json({ data }, { status: 200 });
+    } else if (query.userId) {
+      const data = await prisma.league.findMany({
+        where: {
+          userId: String(query.userId),
+        },
+      });
+
+      return NextResponse.json({ data }, { status: 200 });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json(null, { status: 500 });
+  }
 }
 
 export async function POST(req: NextRequest) {
-	try {
-		const league: League = await req.json();
-		const data = await prisma.league.create({ data: league });
+  try {
+    const league: League = await req.json();
+    const data = await prisma.league.create({ data: league });
 
-		console.log('League posted:', data);
-		return NextResponse.json({ data }, { status: 200 });
-	}
-	catch(error) {
-		console.error('Error:', error);
-		return NextResponse.json(null, { status: 500 });
-	}
+    console.log('League posted:', data);
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json(null, { status: 500 });
+  }
 }
