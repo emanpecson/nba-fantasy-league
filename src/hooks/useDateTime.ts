2d55ec44afec158426b49146726c9e787dfbbbb3
Emanuel Pecson <emanpecson@gmail.com>
2023-11-17 16:18:46 -0800

    feat: :sparkles: add hook for converting date to readable string

---
+++ src/hooks/useDateTime.ts
@@ -0,0 +1,17 @@
+'use client';
+
+export default function useDateTime(date: Date) {
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  let day = '';
+
+  if (date.toLocaleDateString() === today.toLocaleDateString()) day = 'Today';
+  else if (date.toLocaleDateString() === yesterday.toLocaleDateString()) day = 'Yesterday';
+  else day = date.toLocaleDateString();
+
+  return `${day} ${date.toLocaleTimeString([], {
+    timeStyle: 'short',
+  })}`;
+}
