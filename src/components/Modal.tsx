e7b2e8c62fea8aad966b70072026e40c9db1c0b4
Emanuel Pecson <emanpecson@gmail.com>
2023-11-17 16:20:05 -0800

    style: :lipstick: increase modal width

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,19 @@
-import React, { Fragment, useRef, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import React, { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
-export default function Modal({ isOpen, onClose, children }: { isOpen: boolean, onClose: () => void, children: React.ReactNode }) {
-  const cancelButtonRef = useRef(null)
-	
-	return (
+export default function Modal({
+  isOpen,
+  onClose,
+  children,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}) {
+  const cancelButtonRef = useRef(null);
+
+  return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={onClose}>
         <Transition.Child
@@ -15,8 +23,7 @@ export default function Modal({ isOpen, onClose, children }: { isOpen: boolean, 
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+          leaveTo="opacity-0">
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
@@ -29,17 +36,18 @@ export default function Modal({ isOpen, onClose, children }: { isOpen: boolean, 
               enterTo="opacity-100 translate-y-0 sm:scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-							<button type="button" className="hover:bg-gray-200 rounded-full p-1" onClick={onClose} ref={cancelButtonRef}>
-								<XMarkIcon className="h-6 w-6" />
-							</button>
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
+                <button
+                  type="button"
+                  className="hover:bg-gray-200 rounded-full p-1"
+                  onClick={onClose}
+                  ref={cancelButtonRef}>
+                  <XMarkIcon className="h-6 w-6" />
+                </button>
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
-                    <div className="mt-2">
-											{ children }
-                    </div>
+                    <div className="mt-2">{children}</div>
                   </div>
                 </div>
               </Dialog.Panel>
@@ -48,5 +56,5 @@ export default function Modal({ isOpen, onClose, children }: { isOpen: boolean, 
         </div>
       </Dialog>
     </Transition.Root>
-  )
+  );
 }
