1ae0bf694f4bcf24f5d18bcab66f737d4b936c10
Emanuel Pecson <emanpecson@gmail.com>
2023-12-05 15:13:04 -0800

    style: :lipstick: setup layout for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 import RouteButton from '@/components/RouteButton';
 
-export default function Home() {
-	return (
-		<div>
-			<div>
-				<RouteButton route='/login'>
-					Login
-				</RouteButton>
-			</div>
-			<div>
-				<RouteButton route='/signup'>
-					Signup
-				</RouteButton>
-			</div>
-		</div>
-	)
+export default function LandingPage() {
+  return (
+    <div className="flex justify-center h-full">
+      <div>
+        {/* header */}
+        <div className="py-6">
+          <h1 className="text-4xl font-semibold">NBA Fantasy League</h1>
+          <p className="flex justify-center pt-2 text-gray-500">short description</p>
+        </div>
+
+        {/* action buttons */}
+        <div className="flex space-x-1.5 justify-center">
+          <RouteButton route="/login">Login</RouteButton>
+          <RouteButton route="/signup">Sign up</RouteButton>
+        </div>
+
+        {/* app image */}
+      </div>
+      <div className="bg-gray-200 bottom-0 absolute h-60 w-[40rem] rounded-2xl">
+        <p className="flex justify-center place-items-center h-full">placeholder</p>
+      </div>
+    </div>
+  );
 }
--- src/components/RouteButton.tsx
+++ src/components/RouteButton.tsx
@@ -1,16 +1,23 @@
 'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
-export default function RouteButton({ children, route }: { children: React.ReactNode, route: string }) {
-	const router = useRouter();
+export default function RouteButton({
+  children,
+  route,
+}: {
+  children: React.ReactNode;
+  route: string;
+}) {
+  const router = useRouter();
 
-	return (
-		<button 
-			className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-			onClick={() => { router.push(route) }}
-		>
-			{ children }
-		</button>
-	)
+  return (
+    <button
+      className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 w-36 border border-gray-400 rounded-lg"
+      onClick={() => {
+        router.push(route);
+      }}>
+      {children}
+    </button>
+  );
 }
